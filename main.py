d22ba9e94062c5ab1f7c94eb58e4d0387752adcf
Данила Ильин <danila.ilyin2000@gmail.com>
2024-04-04 19:55:58 +0300

    visual

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from datetime import datetime
+import psycopg2
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from app.parser import parse_table
 from app.database import save_to_database
 from app.csv_writer import write_to_csv
@@ -18,6 +23,27 @@ def main():
 
     save_to_database(table_data)
     write_to_csv(table_data, 'table_data.csv')
+
+    conn = psycopg2.connect(dbname='postgres', user='postgres', password='1534', host='localhost', port='5432')
+    query = "SELECT * FROM planned_work"
+    df = pd.read_sql(query, conn)
+    conn.close()
+
+    plt.figure(figsize=(10, 6))
+    sns.countplot(x='type_of_work', data=df)
+    plt.title('Distribution of Types of Work')
+    plt.xlabel('Type of Work')
+    plt.ylabel('Count')
+    plt.xticks(rotation=15)
+    plt.show()
+
+    plt.figure(figsize=(12, 6))
+    sns.countplot(x='region', data=df, order=df['region'].value_counts().index)
+    plt.title('Number of Works by Region')
+    plt.xlabel('Region')
+    plt.ylabel('Count')
+    plt.xticks(rotation=0)
+    plt.show()
 
 if __name__ == "__main__":
     main()
